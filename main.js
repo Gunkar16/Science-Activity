4730782627b0c15560abd386bf4d682739181cab
Gunkar16 <gunkarsinghjohal@gmail.com>
2023-07-02 17:46:52 +0530

    LETS GOOOOO

--- main.js
+++ main.js
@@ -1,18 +1,52 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  console.log(reveals.length);
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;//258
-      var elementTop = reveals[i].getBoundingClientRect().top; // 1 of the .reveal objects top point
-      var elementVisible = 150;
-  
-      if (elementTop < 108) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;//258
+    var elementTop = reveals[i].getBoundingClientRect().top; // 1 of the .reveal objects top point
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
-  
-  window.addEventListener("scroll", reveal);
-  
+}
+window.addEventListener("scroll", reveal);
+
+
+
+
+
+
+
+function pla(lol) {
+  lol.play();
+}
+function paus(lol) {
+  lol.pause();
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
